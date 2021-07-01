f2b56700b867f39daf7fd58146df3663357f00e6
neha <neha.agarwal2@microfocus.com>
2021-07-01 15:18:11 +0530

    added rolling file appender

--- src/main/java/io/cloudslang/web/ScoreWebApplication.java
+++ src/main/java/io/cloudslang/web/ScoreWebApplication.java
@@ -26,7 +26,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
